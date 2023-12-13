a90eb58a3b94c596f8104f7cd251280d15bef759
SidneiArrudaJr <152440120+SidneiArrudaJr@users.noreply.github.com>
2023-12-13 00:41:56 -0300

    att

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirmenu(){
+    var menu = document.getElementById('nav')
+    if (menu.style.display == 'none') {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
